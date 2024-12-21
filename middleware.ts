b3ef966226950c0e52a1c6c6b186573bde25231c
Ahmed <168854600+AhmedymHub@users.noreply.github.com>
2024-12-21 19:55:13 +0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,13 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
 export default clerkMiddleware();
 
 export const config = {
   matcher: [
     '/',  // Public route
-    '/api/webhooks/clerk',
-    '/((?!_next/static|_next/image|favicon.ico|.*\\..*).*)', // Protect all routes except static files
-    '/api/(.*)', // Always apply middleware for API routes
+    '/api/webhooks/clerk',  // Public webhook route
+    '/api/webhooks/stripe',  // Public webhook route
+    '/((?!.+\\.[\\w]+$|_next).*)',  // Matches all dynamic routes except static files
+    '/(api|trpc)(.*)',  // Apply middleware to API routes
   ],
 };
